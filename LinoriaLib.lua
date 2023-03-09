abedbfec294caf5ea470602be31ff315d21f2cef
SOULS <121745347+soulsie@users.noreply.github.com>
2023-03-09 02:38:20 +0200

    Update LinoriaLib.lua

--- LinoriaLib.lua
+++ LinoriaLib.lua
@@ -31,7 +31,7 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(22, 22, 22);
-    AccentColor = Color3.fromRGB(94, 0, 255);
+    AccentColor = Color3.fromRGB(251, 91, 35);
     OutlineColor = Color3.fromRGB(24, 24, 24);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
