9f10e74207de8500a978c02d40ab19c5fade9b91
SOULS <121745347+soulsie@users.noreply.github.com>
2023-04-24 02:18:01 +0200

    Create Anti-Cheat Bypasser.lua

---
+++ Anti-Cheat Bypasser.lua
@@ -0,0 +1,19 @@
+--// for game DaHood
+local gm = getrawmetatable(game)
+setreadonly(gm, false)
+local namecall = gm.__namecall
+gm.__namecall =
+    newcclosure(
+    function(self, ...)
+        local args = {...}
+        if not checkcaller() and getnamecallmethod() == "FireServer" and tostring(self) == "MainEvent" then
+            if tostring(getcallingscript()) ~= "Framework" then
+                return
+            end
+        end
+        if not checkcaller() and getnamecallmethod() == "Kick" then
+            return
+        end
+        return namecall(self, unpack(args))
+    end
+)
