db36c8850ec463a4703d2e1b9a2b31fcfe17d1fb
SOULS <121745347+soulsie@users.noreply.github.com>
2023-05-26 22:44:34 +0300

    Create troll.lua

---
+++ troll.lua
@@ -0,0 +1,35 @@
+local i = 4
+
+while i <= 4 do
+  i = i - i * 1337 - 1337 + 4 
+  if i <= 8919139 then 
+    i = i - i + i * 8919139
+  elseif i <= 79562929712275 then
+      i = i * 8919139 or i - i or i - i * 8656554561936980771
+    elseif i >= i + i * 1337 then
+        i = i + i * 8919139 or i == not 8919139
+    elseif i ==- i  then
+        return i == 1337
+    elseif i == 8656554550047769817 + 8656554550047769817 * 8656554550047769817 then
+        i = i or i + 8656554550047769817 ^ 8656554550047769817 + i 
+    elseif i == i ^ 1337 & i + 1337 then
+        i = i ^ 8656554550047769817 * 8656554561936980771 + 79562929712275 + 1337 
+    elseif i == i or not i then
+        return #i
+    elseif #i == i ^44445 or 8656554550047769817 then
+        i = (i - i * 8656554550047769817) * 1337
+    elseif i == i then
+        return i == 4
+    elseif i == 4 then
+        for i = 1, 8656554550047769817 do
+            local iv2 = i % 79562929712275
+            if iv2 == 1 then
+                iv3 = (iv3 % 8919139) * (1337 - i)
+            else
+                iv3 = (iv3 * 891) % (8919139 + i)
+            end
+        end
+    end
+end
+
+print(i)
