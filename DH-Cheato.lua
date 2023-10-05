b9bd9456a4f0de6f7a415571a62a9bd8739a0776
SOULS <121745347+soulsie@users.noreply.github.com>
2023-10-05 21:28:05 +0300

    a

--- DH-Cheato.lua
+++ DH-Cheato.lua
@@ -5,12 +5,10 @@ old = grm.__namecall
 
 grm.__namecall = newcclosure(function(self, ...)
     local args = {...}  
-
     local methodName = tostring(args[1])
-
     local blockedMethods = {"TeleportDetect", "CHECKER_1", "CHECKER", "GUI_CHECK", "OneMoreTime", "checkingSPEED", "BANREMOTE", "PERMAIDBAN", "KICKREMOTE", "BR_KICKPC", "BR_KICKMOBILE"}
 
-    if table.find(blockedMethods, methodName) then return end
+    if table.find(blockedMethods, methodName) then return end --// doing the funny
 
     return old(self, ...)
 end)
