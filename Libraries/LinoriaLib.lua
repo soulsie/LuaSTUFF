e05a6856e207ecef8168bdf846435b33654d8180
SOULS <121745347+soulsie@users.noreply.github.com>
2023-10-06 23:24:40 +0300

    Update LinoriaLib.lua

--- Libraries/LinoriaLib.lua
+++ Libraries/LinoriaLib.lua
@@ -7,7 +7,7 @@ local RunService = game:GetService('RunService')
 local RenderStepped = RunService.RenderStepped;
 local LocalPlayer = Players.LocalPlayer;
 local Mouse = LocalPlayer:GetMouse();
-
+-- a
 local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 
 local ScreenGui = Instance.new('ScreenGui');
